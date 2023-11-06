0cd2b2c01ef99df7bdb43c4d61e5afd7ebff7b5a
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-11-06 17:11:33 +0300

    init

---
+++ scripts.py
@@ -0,0 +1,62 @@
+import random
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
+
+
+def fix_marks(schoolkid):
+    child_bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    for child_bad_mark in child_bad_marks:
+        child_bad_mark.points = 5
+        child_bad_mark.save()
+
+
+def remove_chastisements(schoolkid):
+    child_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    child_chastisements.delete()
+
+
+list_of_accolades = [
+    'Молодец!',
+    'Отлично!'
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+    'Именно этого я давно ждал от тебя!',
+    'Сказано здорово – просто и ясно!',
+    'Ты, как всегда, точен!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Ты сегодня прыгнул выше головы!',
+    'Я поражен!',
+    'Уже существенно лучше!',
+    'Потрясающе!',
+    'Замечательно!',
+    'Прекрасное начало!',
+    'Так держать!',
+    'Ты на верном пути!',
+    'Здорово!',
+    'Это как раз то, что нужно!',
+    'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой не зря поработали!',
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!'
+]
+
+
+def create_commendation(schoolkid:str, lesson:str):
+    kids = Schoolkid.objects.all()
+    child = kids.get(full_name__contains=schoolkid)
+    all_given_lessons_in_6A = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=lesson)
+    last_given_lesson = all_given_lessons_in_6A[0]
+    Commendation.objects.create(
+        text=random.choice(list_of_accolades),
+        created=last_given_lesson.date,
+        schoolkid=child,
+        subject=last_given_lesson.subject,
+        teacher=last_given_lesson.teacher
+    )
